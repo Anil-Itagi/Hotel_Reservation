84d40623efacbe98bc3e9175b70f08cf0f3c0289
Anil Itagi <itagianil464@gmail.com>
2025-03-24 18:05:07 +0530

    simple withdraw and deposite problem to understand the transaction in JDBC

--- Transaction.java
+++ Transaction.java
@@ -1,23 +1,40 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class Transaction {
     public static void main(String[] args) {
          try {
-            String url = "jdbc:mysql://localhost:3306/hotel_db";
+            String url = "jdbc:mysql://localhost:3306/bank";
             String username = "root";
-            String query="select * from reservations  where reservation_id=? and guest_name=?";
             String password = "appa@123";
+            String withdrawQuery = "update new_bank set balance=balance- ? where account_num=? ";
+            String depositeQuery = "update new_bank set balance=balance+ ? where account_num=? ";
+
             Connection conn = DriverManager.getConnection(url, username, password);
 
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setInt(1, 1);
-            preparedStatement.setString(2, "Anil1");
-            ResultSet res = preparedStatement.executeQuery();
+            conn.setAutoCommit(false);
+            PreparedStatement withdrawStmt = conn.prepareStatement(withdrawQuery);
+            PreparedStatement depositeStmt = conn.prepareStatement(depositeQuery);
 
-            
+            withdrawStmt.setDouble(1, 100.00);
+            withdrawStmt.setString(2, "101");
+  
+            depositeStmt.setDouble(1, 100.00);
+            depositeStmt.setString(2, "100");
+
+
+            int withdrawRowsAffected = withdrawStmt.executeUpdate();
+            int depositeRowsAffected = depositeStmt.executeUpdate();
+
+            if (withdrawRowsAffected > 0 && depositeRowsAffected > 0) {
+                conn.commit();
+                System.out.println("amount transfer successfull");
+            }
+            else {
+                conn.rollback();
+                System.out.println("amount transfer failed");
+            }
 
          }
          catch(Exception e){
